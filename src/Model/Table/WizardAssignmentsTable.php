acc71778ef569c459d375d6145dd6639eb1a40cc
abergen <a.bergen@avendis.com>
2026-04-29 08:45:42 +0200

    ITC-3765 - added raritan kvm wizard to list

--- src/Model/Table/WizardAssignmentsTable.php
+++ src/Model/Table/WizardAssignmentsTable.php
@@ -649,6 +649,14 @@ class WizardAssignmentsTable extends Table {
                 'category'    => ['network', 'hardware'],
                 'active'      => true
             ],
+            'raritan-kvm'                  => [
+                'type_id'     => 'raritan-kvm',
+                'title'       => __('Raritan KVM'),
+                'description' => __('Monitor your Raritan KVM via SNMP.'),
+                'image'       => 'network.png',
+                'category'    => ['linux', 'network', 'hardware'],
+                'active'      => true
+            ],
         ];
 
         $possibleWizardsResult = [];
